c7e7c44e59e55aca913bd3db59bdedcc388683c6
ChrisPitts <cp25393@gmail.com>
2020-02-28 15:26:46 -0600

    Added some comments

--- reader.py
+++ reader.py
@@ -2,43 +2,79 @@ from tkinter import *
 from tkinter import filedialog
 
 
+#   onclick_button_browse_files()
+#
+#   Description
+#       Prompts the user to choose the file
 def onclick_button_browse_files():
-    directory = filedialog.askopenfilename()
-    if len(directory) != 0:
+
+    directory = filedialog.askopenfilename()    # Prompt the user to choose a file
+
+    if len(directory) != 0:     # The user chose a file
+        # Fill in the file address text entry
         entry_file_address.delete(0, END)
         entry_file_address.insert(0, directory)
 
 
+#   generate_output_window()
+#
+#   Description
+#       Generates a window that tells the user what lines
+#       exceed the character max
 def generate_output_window():
     try:
         file_address = entry_file_address.get()
         open(file_address)
-    except Exception:
+    except Exception:   # The user inputted an invalid file address
         error_string.set("File address invalid")
         return
 
     try:
         max_line_length = int(entry_max_length.get())
-    except Exception:
+    except Exception:   # The user inputted an invalid value for max line
         error_string.set("Max line length invalid")
         return
 
-    window_output = Tk()
-    text = Text(window_output)
+    window_output = Tk()    # The window that shows the output
+    text = Text(window_output)  # The field that holds the output string
     text.grid(row=0, column=0, sticky='W')
+
+    # Write the output string to the output window
     text.insert(END, get_excess_lines(file_address, max_line_length))
 
     window_output.mainloop()
 
 
+#   get_excess_lines(file_address, max_line_length)
+#
+#   Parameters
+#       file_address-       (string)    holds the address of the file to be read
+#       max_line_length-    (int)       holds the maximum length of each line
+#
+#   Return
+#       A string containing the lines that exceed the max length
+#       Format:
+#           Line:   #
+#           Characters: <characters in line>
+#           Line text:  <Contents of line>
+#
+#   Description
+#       Returns a string containing information about every line
+#       that exceeds the character limit
 def get_excess_lines(file_address, max_line_length):
     current_line = 1
     return_str = ""
 
+    #   Loop through each line in the file
     for line in open(file_address, 'r').readlines():
+
+        #   Remove all the spaces from the beginning of the line
         while line[0] == ' ':
             line = line[1:]
+
+        #   If the line exceeds the maximum length
         if len(line) > max_line_length:
+            #   Add the line's information to the return string
             return_str = return_str + "Line:\t%i\n" % current_line
             return_str = return_str + "Characters:\t%i\n" % len(line)
             return_str = return_str + "Line Text:\t%s\n" % line
